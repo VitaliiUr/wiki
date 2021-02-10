382c9094f5a43e97a25f87a96c9dbc5d88e5ed4e
VitaliiUr <vitaliyurbanevich@gmail.com>
2021-02-10 14:08:15 +0100

    add script

---
+++ wiki_app.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import wikipedia as wiki
+import re
+import sys
+import argparse
+
+
+def get_random_title():
+    title = wiki.random()
+    print("Random article's title:")
+    print(title)
+
+    ans = input(
+        "Do you want to read it?\n (Press any key if yes or \"n\" if you want to see next suggestion)\n\
+            Press \"q\" to quit")
+
+    if ans in ("n", "next"):
+        return get_random_title()
+    elif ans == "q":
+        print("sorry for that")
+        sys.exit(0)
+    else:
+        return title
+
+
+def search_title(search):
+
+    titles = wiki.search(search)
+    print("We found such articles:\n")
+    print(*[f"\"{t}\","for t in titles[:5]])
+
+    for title in titles:
+        ans = input(
+            "Did you mean \"{}\"?\n(Press any key if yes or \"n\" if you want to see next suggestion)\n\
+                Press \"q\" to quit".format(title))
+        if ans in ("n", "next"):
+            continue
+        elif ans == "q":
+            print("sorry for that")
+            sys.exit(0)
+        else:
+            return title
+
+
+def split_paragraphs(text):
+    pat = re.compile(
+        r"(?:(?:(?:\n+)?)(?:\s?)(?:=+)(?:\s?)(?:[.\w\s]+)(?:\s?)(?:=+)(?:(?:\n+)?))+")
+
+    paragraphs = pat.split(text)
+
+    pat2 = pat = re.compile(
+        r"(?:(?:(?:\n+)?)(?:\s?)(?:=+)(?:\s?)([.\w\s]+)(?:\s?)(?:=+)(?:(?:\n+)?))+")
+
+    titles = list(map(lambda x: x.strip(), ["Summary"] + pat2.findall(text)))
+
+    result = dict(zip(titles, paragraphs))
+    if "References" in result:
+        del result["References"]
+    return result
+
+
+if __name__ == "__main__":
+    global keyPressed
+    keyPressed = None
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--search", type=str,
+                        help="search wiki article by title")
+    args = parser.parse_args()
+
+    if args.search:
+        name = search_title(args.search)
+    else:
+        name = get_random_title()
+
+    print("Article is loading. Please, wait")
+    page = wiki.page(name)
+
+    paragraphs = split_paragraphs(page.content)
+
+    for title in paragraphs:
+        print("\n")
+        print("=== ", title, " ===")
+        print(paragraphs[title])
+        ans = input(
+            "Press any key to proceed, \"q\" to quit")
+        if ans == "q":
+            sys.exit(0)
